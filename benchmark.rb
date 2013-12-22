056ce4f616b4ae4ef48242794d57349b4ba7c43a
ongaeshi <ongaeshi0621@gmail.com>
2013-12-22 13:49:34 +0900

    benchmark.rb TEST_TIMING の仕組みを追加

--- benchmark.rb
+++ benchmark.rb
@@ -7,33 +7,23 @@ GrnArray.tmpdb do |array|
   
   texts = File.read('dummy/dummy1.txt').split
 
-  DATA_NUM = 1000000
+  TEST_TIMING = [100, 1000, 10000, 100000, 1000000]
+  DATA_NUM    = TEST_TIMING[-1]
+  test_index = 0
 
-  puts "-- GrnArray#select --"
-  srand(0)
   DATA_NUM.times.each do |index|
-    puts index if index % 1000 == 0
+    # puts index if index % 1000 == 0
     text = texts[rand(texts.size)]
     array << {text: text}
-  end
-
-  p array.select("しかし").size
-
-  Benchmark.bm(16) do |x|
-    x.report("GrnArray#select") { 100.times { array.select("しかし") } }
-  end
-
-  # puts "-- Array#grep --"
-  srand(0)
-  DATA_NUM.times.each do |index|
-    puts index if index % 1000 == 0
-    text = texts[rand(texts.size)]
     native_array << text
-  end
 
-  p native_array.grep(/しかし/).size
-
-  Benchmark.bm(16) do |x|
-    x.report("Array#grep")      { 100.times { native_array.grep(/しかし/) } }
+    if (array.size == TEST_TIMING[test_index])
+      puts "-- #{array.size} --"
+      Benchmark.bm(16) do |x|
+        x.report("GrnArray#select") { 100.times { array.select("しかし") } }
+        x.report("Array#grep")      { 100.times { native_array.grep(/しかし/) } }
+      end
+      test_index += 1
+    end
   end
 end
