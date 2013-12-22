3a066168e9675203547872cbe76659433b9b6b27
ongaeshi <ongaeshi0621@gmail.com>
2013-12-22 13:21:55 +0900

    テストデータをテキストファイルでは無くつぶやき(風の)テキストに変更。

--- benchmark.rb
+++ benchmark.rb
@@ -5,30 +5,33 @@ require 'benchmark'
 GrnArray.tmpdb do |array|
   native_array = []
   
-  texts = []
-  texts << File.read('dummy/dummy1.txt')
-  texts << File.read('dummy/dummy2.txt')
-  texts << File.read('dummy/dummy3.txt')
+  texts = File.read('dummy/dummy1.txt').split
 
-  DATA_NUM = 10000
+  DATA_NUM = 1000000
 
   puts "-- GrnArray#select --"
+  srand(0)
   DATA_NUM.times.each do |index|
     puts index if index % 1000 == 0
-    text = texts[rand(3)]
+    text = texts[rand(texts.size)]
     array << {text: text}
   end
+
+  p array.select("しかし").size
 
   Benchmark.bm(16) do |x|
     x.report("GrnArray#select") { 100.times { array.select("しかし") } }
   end
 
-  puts "-- Array#grep --"
+  # puts "-- Array#grep --"
+  srand(0)
   DATA_NUM.times.each do |index|
     puts index if index % 1000 == 0
-    text = texts[rand(3)]
+    text = texts[rand(texts.size)]
     native_array << text
   end
+
+  p native_array.grep(/しかし/).size
 
   Benchmark.bm(16) do |x|
     x.report("Array#grep")      { 100.times { native_array.grep(/しかし/) } }
