41aa1d953e253136dc46e3fa665bf0c0a001a7bd
ongaeshi <ongaeshi0621@gmail.com>
2013-12-23 17:58:09 +0900

    Divide input mode & search mode

--- aozora.rb
+++ aozora.rb
@@ -11,24 +11,33 @@ end
 array = GrnArray.new("db/aozora.db")
 
 if array.empty?
-  Find.find("#{ENV["HOME"]}/Downloads/tmp/青空文庫/夏目漱石")  do |filename|
+  # Input
+  if ARGV.empty?
+    puts "aozora.rb [input_dir]"
+    exit
+  end
+
+  Find.find(File.expand_path ARGV[0])  do |filename|
     if File.file? filename
       array << {filename: filename, text: read_file(filename)}
     end
-    p array.size if array.size % 1000 == 0
+    puts "Input : #{array.size}" if array.size > 0 && array.size % 100 == 0
   end
   puts "Input complete : #{array.size} files"
-end
 
-unless ARGV.empty?
-  results = array.select(ARGV.join(" "))
-  puts "#{results.size} matches"
-  snippet = results.snippet_text
+else
+  # Search
+  unless ARGV.empty?
+    results = array.select(ARGV.join(" "))
+    puts "#{results.size} matches"
+    snippet = results.snippet_text
 
-  results.each do |record|
-    puts "--- #{record.filename} ---"
-    snippet.execute(record.text).each do |segment|
-      puts segment.gsub("\n", "")
+    results.each do |record|
+      puts "--- #{record.filename} ---"
+      snippet.execute(record.text).each do |segment|
+        puts segment.gsub("\n", "")
+      end
     end
   end
 end
+
